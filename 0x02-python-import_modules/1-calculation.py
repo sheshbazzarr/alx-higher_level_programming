67762992852ca8a3c18f11390b05f8aa888470f0
Daniel Demerw <99654542+danieldemerw@users.noreply.github.com>
2024-03-14 20:49:17 -0500

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """
-    import functions from modules
+    import functions from modules update
     """
 
     from calculator_1 import add, sub, mul, div
