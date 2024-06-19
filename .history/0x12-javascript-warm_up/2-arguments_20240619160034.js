18511af14c63a78a7aed73b5147937ea02c92bf6
danieldemerw <demerwdani@gmail.com>
2024-06-19 16:02:25 -0500

    2-arguments.js

---
+++ .history/0x12-javascript-warm_up/2-arguments_20240619160034.js
@@ -0,0 +1,5 @@
+
+const myArg=process.argv.length;
+if(myArg==2){
+    
+}
---
+++ .history/0x12-javascript-warm_up/2-arguments_20240619160219.js
@@ -0,0 +1,9 @@
+
+const myArg=process.argv.length;
+if(myArg==2){
+    console.log('No argument');
+}else if(myArg===3){
+    console.log('Argument found');
+}else{
+    console.log('Arguments found');
+}
