a5be1a7f76179dd7e02bff2a9ffe7582b052a861
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-07-31 06:10:11 -0500

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,20 @@
 #!/usr/bin/python3
-"""
-script to list all states from db hbtn_0e_usa
-takes 3 args mysql username, mysql passwd, db name
-"""
-
-import MySQLdb
+#!/usr/bin/python3
+'''
+lists all states from the database hbtn_0e_0_usa
+'''
 import sys
+import MySQLdb
+
 
 if __name__ == '__main__':
-    args = sys.argv
-    if len(args) != 4:
-        print("Usage: {} username password database_name".format(args[0]))
-        exit(1)
-    username = args[1]
-    password = args[2]
-    data = args[3]
-    db = MySQLdb.connect(host='localhost', user=username,
-                         passwd=password, db=data, port=3306)
-    cur = db.cursor()
-    num_rows = cur.execute('SELECT * FROM states ORDER BY states.id;')
-    rows = cur.fetchall()
-    for row in rows:
+    conn = MySQLdb.connect(
+        host="localhost", port=3306, user=sys.argv[1],
+        passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
     cur.close()
-    db.close()
+    conn.close()
