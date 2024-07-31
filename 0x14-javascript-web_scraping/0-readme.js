51b49ed677b81310a22ea26c5c9112cae53026b3
sheshbazzarr <demerwdani@gmail.com>
2024-07-30 19:23:23 -0500

    commit

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,13 @@
 #!/usr/bin/node
-const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
-});
+// Read from file
+
+const filses = require('fs');
+filses.readFile(process.argv[2], 'utf-8',
+  (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  });
   
