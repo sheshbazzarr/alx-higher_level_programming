98950cbde0206c752c78bcc925366c91080c074f
sheshbazzarr <demerwdani@gmail.com>
2024-07-31 12:36:24 -0500

    Add Python script to fetch URL and display response body

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+sends a request to the passed URL and displays
+the body of the response (decoded in utf-8) while
+managing the error exception using urllib.error.HTTPError
+"""
+
+import urllib.request
+import sys
+
+if __name__ == '__main__':
+    try:
+        url = sys.argv[1]
+        request = urllib.request.Request(url)
+        with urllib.request.urlopen(request) as Response:
+            print(Response.read().decode('UTF-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
+    except Exception:
+        pass
