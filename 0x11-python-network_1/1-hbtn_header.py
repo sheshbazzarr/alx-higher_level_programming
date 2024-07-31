74e783d3da585e9975ffdbb262226093cf648c57
sheshbazzarr <demerwdani@gmail.com>
2024-07-31 12:28:12 -0500

    Add script to fetch and display X-Request-Id from response headers

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Script that sends a request
+"""
+
+from urllib.request import urlopen
+from sys import argv
+
+
+if __name__ == '__main__':
+    url = argv[1]
+    with urlopen(url) as response:
+        info = response.info()
+        print(info['X-Request-Id'])
