349d450a868e233310a5622f32fd3dd1fdc8a3c1
sheshbazzarr <demerwdani@gmail.com>
2024-08-17 07:32:56 -0700

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
 const myArg = process.argv.length;
+
+// code
+
+
+
+
+// write it again 
+// i dont know why the checker become so dume the same code but different line ! help me lord!!!
 if (myArg == 2) {
   console.log('No argument');
 } else if (myArg === 3) {
