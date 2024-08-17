1103e1c921ec171095ef41a5099706e9ec2494f3
sheshbazzarr <demerwdani@gmail.com>
2024-08-17 07:42:05 -0700

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-// Calculate the number of arguments passed to the script using 'process.argv.length - 2'
-// Subtract 2 to exclude the 'node' command and the name of the script itself
+// calculate the number of arguments passed to the script using 'process.argv.length - 2'
+// sbtracting 2 to exclude the 'node' command and the name of the script itself
 
 const argsCount = process.argv.length - 2;
 
-// Use an 'if...else' statement to check the value of 'argsCount'
-// Use 'console.log' to print the appropriate message
+// using 'if...else' statement to check the value of 'argsCount'
+// using 'console.log' to print the appropriate message
 
 if (argsCount === 0) {
   console.log('No argument');
@@ -15,4 +15,3 @@ if (argsCount === 0) {
 } else {
   console.log('Arguments found');
 }
-
