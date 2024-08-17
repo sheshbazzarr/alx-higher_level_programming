42e635e237e4208fed4eb18e621873c36f88328f
sheshbazzarr <demerwdani@gmail.com>
2024-08-17 07:20:17 -0700

    update

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
-const myVar = 'C is fun';
-console.log(myVar);
-const myVar2 = 'Python is cool';
-console.log(myVar2);
-const myVar3 = 'JavaScript is amazing';
-console.log(myVar3);
+// a script that prints 3 lines
+
+// created constant variables, each with the desired message.
+const line1 = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'JavaScript is amazing';
+
+// the console.log() function is used three times to print each line to the console, one at a time.
+console.log(line1);
+console.log(line2);
+console.log(line3);
 
