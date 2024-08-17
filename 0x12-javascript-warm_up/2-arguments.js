f0f25ffec1f76146bab59e73e2b9d5090436756b
sheshbazzarr <demerwdani@gmail.com>
2024-08-17 07:37:41 -0700

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
-const myArg = process.argv.length;
 
-// code
+// Calculate the number of arguments passed to the script using 'process.argv.length - 2'
+// Subtract 2 to exclude the 'node' command and the name of the script itself
 
+const argsCount = process.argv.length - 2;
 
+// Use an 'if...else' statement to check the value of 'argsCount'
+// Use 'console.log' to print the appropriate message
 
-
-// write it again 
-// i dont know why the checker become so dume the same code but different line ! help me lord!!!
-if (myArg == 2) {
+if (argsCount === 0) {
   console.log('No argument');
-} else if (myArg === 3) {
+} else if (argsCount === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
+
