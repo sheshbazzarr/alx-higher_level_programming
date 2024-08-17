e85af20c96ea86dad0c8e5aaecc11370914b467b
sheshbazzarr <demerwdani@gmail.com>
2024-08-17 07:56:24 -0700

    4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
-const myArg = process.argv[2];
-const myArg2 = process.argv[3];
-console.log(myArg + ' is ' + myArg2);
+// The 'process.argv' array contains the command-line arguments passed to the script
+// Access the third and fourth elements (process.argv[2] and process.argv[3]) to retrieve the arguments to be printed.
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+// Use template literals (backticks) to interpolate the values of 'arg1' and 'arg2' into the output string.
+
+console.log(`${arg1} is ${arg2}`);
+
